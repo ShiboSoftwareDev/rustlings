31425dd49926afc9569ec161ac424fb9f510b32a
Shibo <shibosoftwaredev@gmail.com>
2025-03-05 11:59:18 +0200

    completed macros

--- exercises/21_macros/macros3.rs
+++ exercises/21_macros/macros3.rs
@@ -1,7 +1,8 @@
 // TODO: Fix the compiler error without taking the macro definition out of this
 // module.
-mod macros {
-    macro_rules! my_macro {
+pub mod macros {
+    #[macro_export]
+   macro_rules! my_macro {
         () => {
             println!("Check out my macro!");
         };
